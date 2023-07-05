6e7034a7227b7155af3c0068fa3f4bf4484602ee
furkan <furkansimw@gmail.com>
2023-07-05 15:21:52 +0300

    e

--- src/pages/[id]/index.jsx
+++ src/pages/[id]/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
-const Page = () => {
+const Page = ({ data }) => {
   return (
-    <div>
-      <h1>Page</h1>
+    <div className="place">
+      <div className="header"></div>
+      <main></main>
+      <div className="reviews"></div>
+      <div className="map"></div>
     </div>
   );
 };
 
 export default Page;
+
+export const getServerSideProps = () =>
+  fetch(
+    (process.env.NODE_ENV === "production"
+      ? process.env.URL
+      : "http://localhost:3000") +
+      "/api" +
+      "/place"
+  )
+    .then((r) => r.json())
+    .then((r) => ({ props: { data: r } }));
