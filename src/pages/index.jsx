8407f70c67f9c352c16e67113c6043cbd498cbdc
furkan <furkansimw@gmail.com>
2023-07-05 03:09:20 +0300

    g

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -116,6 +116,8 @@ const Home = ({ data }) => {
                       slidesPerView={1}
                       pagination={{
                         clickable: true,
+                        dynamicBullets: true,
+                        dynamicMainBullets: 8,
                       }}
                       navigation={{
                         nextEl: `.swiper-button-nextx${result.listing.id}`,
@@ -154,7 +156,32 @@ const Home = ({ data }) => {
                       <NextIcon />
                     </button>
                   </div>
-                  <p>{result.listing.city}</p>
+                  <div className="text">
+                    <div className="up">
+                      <p className="city">{result.listing.city}</p>
+                      <p className="rate">
+                        <StarIcon />
+                      </p>
+                    </div>
+                    <div className="view">
+                      {result.listing.structuredContent.mapCategoryInfo[0].body}
+                    </div>
+                    <div className="date">
+                      {
+                        result.listing.structuredContent.mapSecondaryLine[0]
+                          .body
+                      }
+                    </div>
+                    <div className="price">
+                      <span>
+                        {
+                          result.pricingQuote.structuredStayDisplayPrice
+                            .primaryLine.price
+                        }{" "}
+                      </span>
+                      night
+                    </div>
+                  </div>
                 </Link>
               </li>
             );
@@ -329,5 +356,24 @@ const PrevIcon = () => (
     overflow="visible"
   >
     <path fill="none" d="M20 28 8.7 16.7a1 1 0 0 1 0-1.4L20 4"></path>
+  </svg>
+);
+
+const StarIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    aria-hidden="true"
+    role="presentation"
+    focusable="false"
+    display="block"
+    height="12px"
+    width="12px"
+    fill="currentcolor"
+  >
+    <path
+      fill-rule="evenodd"
+      d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"
+    ></path>
   </svg>
 );
