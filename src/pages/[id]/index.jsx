ddad855a412390946f46bd39b120aa6903bba5ce
furkan <furkansimw@gmail.com>
2023-07-05 17:43:07 +0300

    layout done

--- src/pages/[id]/index.jsx
+++ src/pages/[id]/index.jsx
@@ -1,12 +1,106 @@
+import Link from "next/link";
 import React from "react";
+import {
+  GlobalIcon,
+  Logo,
+  MenuIcon,
+  MiniLogo,
+  PersonIcon,
+  SearchIcon,
+} from "..";
 
 const Page = ({ data }) => {
   return (
     <div className="place">
-      <div className="header"></div>
-      <main></main>
-      <div className="reviews"></div>
-      <div className="map"></div>
+      <header>
+        <div className="upside">
+          <Link className="logo" href={"/"}>
+            <Logo />
+            <MiniLogo />
+          </Link>
+          <div className="middle">
+            <p>Anywhere</p>
+            <p className="m">Any week</p>
+            <p className="l">Add guest</p>
+            <button>
+              <SearchIcon />
+            </button>
+          </div>
+          <div className="right">
+            <button className="ayh">Airbnb your home</button>
+            <button className="global">
+              <GlobalIcon />
+            </button>
+            <button className="a">
+              <MenuIcon />
+              <PersonIcon />
+            </button>
+          </div>
+        </div>
+      </header>
+      <div className="content">
+        <main>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1> <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1> <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1> <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1> <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1> <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1> <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+          <h1>Main</h1>
+        </main>
+        <div className="reviews"></div>
+        <div className="map"></div>
+      </div>
     </div>
   );
 };
